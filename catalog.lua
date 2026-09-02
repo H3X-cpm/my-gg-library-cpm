77d0360281a90ef26ecfd0c26f9600802d4557be
H3X-cpm <not.snkzz@gmail.com>
2026-09-02 18:16:21 +0100

    catalog.lua

---
+++ catalog.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    id = "script_one",
+    title = "Script One",
+    version = "1.0",
+    url = "https://raw.githubusercontent.com/H3X-cpm/my-gg-library-cpm/main/script_one.lua"
+  }
+}
